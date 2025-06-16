b40b62eaead10ca2691908a45815e2aed3e1ac23
mohammad bakur <assaa1122a@gmail.com>
2025-06-16 11:37:52 +0300

    CD workflow with new controller to Render Cloud

---
+++ backend/src/main/java/com/moba/PinPongController.java
@@ -0,0 +1,14 @@
+package com.moba;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PinPongController {
+    record PinPong(String result) {}
+
+    @GetMapping("/ping")
+    public PinPong getPinPong() {
+        return new PinPong("pong");
+    }
+}
