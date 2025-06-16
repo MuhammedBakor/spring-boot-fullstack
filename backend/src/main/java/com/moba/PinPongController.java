a3f0eb50da5ecaabcfa73617026b481685a6923e
mohammad bakur <assaa1122a@gmail.com>
2025-06-16 17:08:23 +0300

    added counter to PingPong

--- backend/src/main/java/com/moba/PinPongController.java
+++ backend/src/main/java/com/moba/PinPongController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PinPongController {
+
+    private static int COUNTER = 0;
     record PinPong(String result) {}
 
     @GetMapping("/ping")
     public PinPong getPinPong() {
-        return new PinPong("pong");
+        return new PinPong("Pong %s".formatted(++COUNTER));
     }
 }
