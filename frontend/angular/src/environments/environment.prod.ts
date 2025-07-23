b7a95c9e8a162fbaa6cb327149627748c7d0e2dc
mohammad bakur <assaa1122a@gmail.com>
2025-07-23 16:38:10 +0300

    Angular prod environment config

---
+++ frontend/angular/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  api: {
+    baseUrl: 'https://customer-management-api-3y8d.onrender.com',
+    authUrl: 'api/v1/auth/login',
+    customerUrl: 'api/v1/customers',
+  }
+};
