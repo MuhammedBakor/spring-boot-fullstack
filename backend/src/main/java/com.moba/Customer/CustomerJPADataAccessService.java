322e01eff4bce49029d2bf48164323250e7f7e29
MuhammedBakor <53197043+MuhammedBakor@users.noreply.github.com>
2025-07-24 10:26:00 +0300

    Angular cicd (#3)
    * Enroll to Enrol
    
    * add limit 1000 to queries

--- backend/src/main/java/com.moba/Customer/CustomerJPADataAccessService.java
+++ backend/src/main/java/com.moba/Customer/CustomerJPADataAccessService.java
@@ -1,5 +1,7 @@
 package com.moba.Customer;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -19,7 +21,8 @@ public class CustomerJPADataAccessService implements CustomerDao{
 
     @Override
     public List<Customer> selectAllCustomers() {
-        return customerRepository.findAll();
+        Page<Customer> page = customerRepository.findAll(Pageable.ofSize(1000));
+        return page.getContent();
     }
 
     @Override
