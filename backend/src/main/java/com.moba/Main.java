e54c374102e2cde99ec0360946a48e06479508f1
mohammad bakur <assaa1122a@gmail.com>
2025-07-25 18:03:43 +0300

    Adding simple aws s3 config and service configurations class and uploading and getting a simple object from s3.

--- backend/src/main/java/com.moba/Main.java
+++ backend/src/main/java/com.moba/Main.java
@@ -5,6 +5,7 @@ import com.github.javafaker.Name;
 import com.moba.Customer.Customer;
 import com.moba.Customer.CustomerRepository;
 import com.moba.Customer.Gender;
+import com.moba.s3.S3Service;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,26 +27,40 @@ public class Main {
     @Bean
     CommandLineRunner runner(
             CustomerRepository customerRepository,
-            PasswordEncoder passwordEncoder) {
+            PasswordEncoder passwordEncoder,
+            S3Service s3Service) {
         return args -> {
 
-            Faker faker = new Faker();
-            Random random = new Random();
-            Name name = faker.name();
-            String firstName = name.firstName();
-            String lastName = name.lastName();
-            int age = random.nextInt(16, 99);
-            Gender gender = age % 2 == 0 ? Gender.MALE : Gender.FEMALE;
+            //createRandomCustomer(customerRepository, passwordEncoder);
+            s3Service.putObject(
+                    "put the exact buket name in aws s3",
+                    "foo",
+                    "Helo World".getBytes());
 
-            String email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@moba.com";
-            Customer customer = new Customer(
-                    firstName +" "+ lastName,
-                    email,
-                    passwordEncoder.encode("password"), age,
-                    gender);
+            byte[] obj = s3Service.getObject("put the exact buket name in aws s3",
+                    "foo");
 
-            customerRepository.save(customer);
-            System.out.println(email);
+            System.out.println("Good to go" + new String(obj));
         };
+    }
+
+    private static void createRandomCustomer(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+        Faker faker = new Faker();
+        Random random = new Random();
+        Name name = faker.name();
+        String firstName = name.firstName();
+        String lastName = name.lastName();
+        int age = random.nextInt(16, 99);
+        Gender gender = age % 2 == 0 ? Gender.MALE : Gender.FEMALE;
+
+        String email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@moba.com";
+        Customer customer = new Customer(
+                firstName +" "+ lastName,
+                email,
+                passwordEncoder.encode("password"), age,
+                gender);
+
+        customerRepository.save(customer);
+        System.out.println(email);
     }
 }
